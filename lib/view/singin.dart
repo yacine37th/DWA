71eb3245720e79da0ea54336fe9ebe37055abacf
yacine37th <y.titouah@esi-sba.dz>
2024-01-08 16:43:31 +0100

    firebase configuration fixed

---
+++ lib/view/singin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Center(
+      child: Text("eded")
+     ),
+    );
+  }
+}
